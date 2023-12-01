a1422f294209da1d39dcf823899073af1446f746
iamgopinathan <gopinathan.l@vajraglobal.com>
2023-12-01 12:48:14 +0530

    newfile

---
+++ lib/pages/secondpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Secondpage extends StatefulWidget {
+  const Secondpage({super.key});
+
+  @override
+  State<Secondpage> createState() => _SecondpageState();
+}
+
+class _SecondpageState extends State<Secondpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
